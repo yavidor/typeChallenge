d62bdd164aec949b2f68cd09a06cca2b635785f8
yavidor <yonatan@avidor.org>
2024-03-30 17:47:58 +0300

    Fixed isodd

--- 30301-medium-isodd.ts
+++ 30301-medium-isodd.ts
@@ -10,4 +10,4 @@ type cases = [
 
 
 // ============= Your Code Here =============
-type IsOdd<T extends number> = `${T}` extends `${string}${1 | 3 | 5 | 7 | 9}` ? true : false 
+type IsOdd<T extends number> = `${T}` extends `${number | ''}${1 | 3 | 5 | 7 | 9}` ? true : false 
