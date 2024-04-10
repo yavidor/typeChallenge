cad672716fa418964e7b33e1e91497153c99a04d
yavidor <yonatan@avidor.org>
2024-04-10 22:09:39 +0300

    append ot object, isnever, remove index signature, drop char, mutable, shit, reverse, flip, zip

---
+++ 01367-medium-remove-index-signature.ts
@@ -0,0 +1,35 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type Foo = {
+  [key: string]: any
+  foo(): void
+}
+
+type Bar = {
+  [key: number]: any
+  bar(): void
+  0: string
+}
+
+const foobar = Symbol('foobar')
+type FooBar = {
+  [key: symbol]: any
+  [foobar](): void
+}
+
+type Baz = {
+  bar(): void
+  baz: string
+}
+
+type cases = [
+  Expect<Equal<RemoveIndexSignature<Foo>, { foo(): void }>>,
+  Expect<Equal<RemoveIndexSignature<Bar>, { bar(): void; 0: string }>>,
+  Expect<Equal<RemoveIndexSignature<FooBar>, { [foobar](): void }>>,
+  Expect<Equal<RemoveIndexSignature<Baz>, { bar(): void; baz: string }>>,
+]
+
+
+// ============= Your Code Here =============
+type RemoveIndexSignature<T extends Record<PropertyKey, any>> = { [K in keyof T as string extends K ? never : symbol extends K ? never : number extends K ? never : K]: T[K] }
