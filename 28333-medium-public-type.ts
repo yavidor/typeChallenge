49d699bdab161212d9aada22f820cdb3d0b63d1d
yavidor <yonatan@avidor.org>
2024-04-15 19:37:52 +0300

    filter, public

---
+++ 28333-medium-public-type.ts
@@ -0,0 +1,16 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<PublicType<{ a: number }>, { a: number }>>,
+  Expect<Equal<PublicType<{ _b: string | bigint }>, {}>>,
+  Expect<Equal<PublicType<{ readonly c?: number }>, { readonly c?: number }>>,
+  Expect<Equal<PublicType<{ d: string; _e: string }>, { d: string }>>,
+  Expect<Equal<PublicType<{ _f: () => bigint[] }>, {}>>,
+  Expect<Equal<PublicType<{ g: '_g' }>, { g: '_g' }>>,
+  Expect<Equal<PublicType<{ __h: number; i: unknown }>, { i: unknown }>>,
+]
+
+
+// ============= Your Code Here =============
+type PublicType<T extends Record<any, any>> = { [K in keyof T as K extends `_${string}` ? never : K]: T[K]}
