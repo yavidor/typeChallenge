3ad584d1b33ba0a176184b838585d412149153b3
yavidor <yonatan@avidor.org>
2024-04-26 22:28:48 +0300

    objectentries

---
+++ 02946-medium-objectentries.ts
@@ -0,0 +1,22 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+interface Model {
+  name: string
+  age: number
+  locations: string[] | null
+}
+
+type ModelEntries = ['name', string] | ['age', number] | ['locations', string[] | null]
+
+type cases = [
+  Expect<Equal<ObjectEntries<Model>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<Partial<Model>>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<{ key?: undefined }>, ['key', undefined]>>,
+  Expect<Equal<ObjectEntries<{ key: undefined }>, ['key', undefined]>>,
+  Expect<Equal<ObjectEntries<{ key: string | undefined }>, ['key', string | undefined]>>,
+]
+
+
+// ============= Your Code Here =============
+type ObjectEntries<T> = { [K in keyof T]-?: [K, Required<T>[K] extends never ? undefined : Required<T>[K]] }[keyof T]
